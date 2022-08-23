e139b429b0f5fadb7d5b677a5dfb860f4296ced6
Jack <jackyexuetong@163.com>
2022-08-23 14:58:05 +0800

    添加注释

--- self-localization.py
+++ self-localization.py
@@ -1,20 +1,23 @@
 import rssi
 from rssi import RSSI_Localizer
 
+#ifconfig查看无线网口的名字
 interface = 'wlp2s0'
 rssi_scanner = rssi.RSSI_Scan(interface)
 
-ssids = ['TP-LINK_FF42','25A802','605']
+ssids = ['TP-LINK_FF42','25A802','605'] # wifi名称
 
 ap_info = rssi_scanner.getAPinfo(networks=ssids, sudo=True)
 rssi_values = [ap['signal'] for ap in ap_info]
 
+# 至少需要三个wifi节点
 accessPoints = [{
      'signalAttenuation': 3, 
      'location': {
          'y': 1, 
          'x': 1
      }, 
+     # 暂时没搞懂reference是啥意思
      'reference': {
          'distance': 4, 
          'signal': -50
